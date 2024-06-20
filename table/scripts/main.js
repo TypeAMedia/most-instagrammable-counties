cbf39df0de0e0e661aaf68366890675e9ae91524
mtsuts <mariamtsuts@gmail.com>
2024-06-20 13:45:08 +0400

    almost finished

--- table/scripts/main.js
+++ table/scripts/main.js
@@ -1,127 +1,144 @@
 class App {
 	constructor() {
-		this.zoomExtent = [0.9, 8]
-		this.currentScale = 1
-		this.zoomStep = 0.5
-
+		this.sortType = 'Towns and villages'
+		this.showMore = false;
+		this.tableData = []
+		this.attr = 'all'
 		this.loadDataAndInit()
-
-		let timer = null
-
-		d3.select(window).on('resize.map', () => {
-			if (timer) clearTimeout(timer)
-			timer = setTimeout(() => {
-				this.map?.resize()
-			}, 100)
-		})
 	}
 
 	async loadDataAndInit() {
 		try {
-			const [newData] = await Promise.all([
-				d3.csv('./data/new-data.csv', d3.autoType)
+			const [countrysideData] = await Promise.all([
+				d3.csv('./data/countryside.csv', d3.autoType)
 			])
 
-			this.data = newData
+			this.data = countrysideData
 
+
+			// Table headers
 			const headers = [
 				{
 					label: 'Rank',
 					icon: './images/rank.svg',
-					width: '15%',
+					width: '8%',
 					fieldValue: 'Rank'
 				},
 				{
 					label: 'Location',
 					icon: './images/location.svg',
-					width: '25%',
-					fieldValue: 'County'
+					width: '20%',
+					fieldValue: 'Name of location'
 				},
 				{
 					label: "Location Type",
 					icon: './images/type.svg',
-					width: '25%',
-					fieldValue: 'Total Instagram hashtags'
+					width: '40%',
+					fieldValue: 'Type'
 				},
 				{
 					label: 'Hashtags',
 					icon: './images/hashtags.svg',
-					width: '35%',
-					fieldValue: 'Town'
+					width: '25%',
+					fieldValue: 'Number of instagram hashtags'
 				}
 			]
-			const regionObj = Array.from(new Set(newData.slice().map((d) => d.Region)))
-			console.log(regionObj)
 
-
-			const listOptions = d3.rollups(
-				newData,
-				arr => arr
-					.sort((a, b) => {
-						return d3.ascending(a.County, b.County)
-					})
-					.map(d => ({
-						label: d.County,
-						value: d.County,
-					})),
-				d => d.Region
-			).map((d, i) => ({
-				label: d[0],
-				id: i + 1,
-				choices: d[1]
-			})).sort((a, b) => regionObj.indexOf(a.label) - regionObj.indexOf(b.label))
+			// Dropdown list
 			const list = [
 				{
-					label: 'Town',
-					value: 'Towns',
+					label: `<div> <img src=${'./images/town.svg'} <div> Towns and Villages </div> </div>`,
+					value: 'Towns and villages',
 					icon: './images/town.svg'
 				},
 				{
-					label: 'Country Estates',
+					label: `<div> <img src=${'./images/estates.svg'} <div> Country Estates </div> </div>`,
 					value: 'Country estates',
 					icon: './images/estates/.svg'
 				},
 				{
-					label: 'Great Trail',
-					value: 'Great trail',
+					label: `<div> <img src=${'./images/greatTrail.svg'} <div> Great Trail </div> </div>`,
+					value: 'Great Trails',
 					icon: './images/greatTrail.svg'
 				},
 				{
-					label: 'Historic Castles',
-					value: 'Historic castles',
+					label: `<div> <img src=${'./images/castles.svg'} <div> Historic Castles </div> </div>`,
+					value: 'Castles',
 					icon: './images/castles.svg'
 				},
 				{
-					label: 'Nature Reserves',
+					label: `<div> <img src=${'./images/reserves.svg'} <div> Nature Reserves </div> </div>`,
 					value: 'Nature reserves',
 					icon: './images/reserves.svg'
 				}
 			]
 
+			this.tableData = countrysideData
+
+			// Initial table
+			drawTable(headers, this.tableData, this.attr || 'all', this.sortType || 'Towns and villages')
+
+			// Handle show more button
+			d3.select('#show_more')
+				.on('click', function () {
+					this.showMore = !this.showMore
+					if (this.showMore) {
+						d3.select(this).html('Show less')
+						this.tableData = countrysideData
+					} else {
+						d3.select(this).html('Show more')
+						this.tableData = countrysideData.slice(0, 10)
+					}
+					drawTable(headers, this.tableData, this.attr || 'all', this.sortType || 'Towns and villages')
+				})
+
+			//Handle dropdown
 			this.choice = initDropdown({
-				searchPlaceholderValue: 'Search',
 				list: list,
 				id: '#city_select',
-				cb: county => {
-					this.map.highlightTooltip(county)
+				cb: type => {
+					this.sortType = type
+					drawTable(headers, this.tableData, this.attr || 'all', type)
 				},
-				searchEnabled: true,
 			})
 
-			drawTable(headers, newData)
+			// Handle country boxes 
+			const countryBoxes = d3.selectAll('.country-box')
+			countryBoxes.on('click', (e) => {
+				countryBoxes.classed('active', false)
+				d3.select(e.target).classed('active', true)
+				this.attr = d3.select(e.target).attr('data-target')
+				drawTable(headers, this.tableData, this.attr, this.sortType)
+			})
+
 		} catch (e) {
 			console.error(e)
 		}
 	}
 }
 
-function drawTable(headers, data) {
+// Draw Table
+function drawTable(headers, data, country, type) {
 	const table = d3.select('#table')
 
-	const filteredData = data.filter((d) => d.Rank !== null)
-		.sort((a, b) => a.Rank - b.Rank)
-		.filter((d, index) => index <= 9)
-
+	const filteredData = data
+		.filter((d) => {
+			if (country === 'all') {
+				return true;
+			} else {
+				return d.Country === country
+			}
+		})
+		.filter((d) => d.Type === type)
+		.slice(0, 10)
+		.sort((a, b) => b['Number of instagram hashtags'] - a['Number of instagram hashtags'])
+		.map((d, index) => {
+			return {
+				...d,
+				Rank: index + 1
+			}
+		})
+	console.log(filteredData)
 
 	// Create headers
 	const header = table
@@ -132,8 +149,10 @@ function drawTable(headers, data) {
 		.style('flex', (d) => `2 2 ${d.width}`)
 		.style('text-align', 'center')
 		.html(d => `
+		<div class='header'>
 			<img class='table-icon' src='${d.icon}'/>
 			<div class='table-label'> ${d.label} </div>
+			</div>
 		`)
 
 	header.each(function (headerData) {
@@ -150,16 +169,20 @@ function drawTable(headers, data) {
 			.join('div')
 			.attr('class', `table-cell ${headerData.fieldValue}`)
 			.html(d => {
-				if (headerData.fieldValue === 'Total Instagram hashtags') {
+				if (headerData.fieldValue === 'Number of instagram hashtags') {
 					return formatThousand(d)
 				} else if (headerData.fieldValue === 'Rank') {
 					return `${d}${get_ordinal_suffix(d)}`
-				} else return d
+				} else if (headerData.fieldValue === 'Name of location') {
+					const place = filteredData.find((x) => x['Name of location'] === d)
+					return `
+					<div class='place'> <div>${d} </div> <div class='county'> ${place.County},  ${place.Country} </div> </div> `
+				}
+				else return d
 			})
 	})
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const app = new App()
-	window.app = app
+	new App()
 })
